637b6f588ae848707325bfc7cf0578eb7ec8186d
nitsuga5124 <nitsuga5124@gmail.com>
2020-12-12 19:35:58 +0100

    Split main file 	modified:   src/main.rs 	new file:   src/routes/login.rs 	new file:   src/routes/mod.rs

--- src/main.rs
+++ src/main.rs
@@ -7,183 +7,25 @@ extern crate tracing;
 pub mod error;
 pub mod model;
 pub mod utils;
+pub mod routes;
 
-use crate::error::*;
-use crate::model::*;
-use crate::utils::tokens::gen_token;
+use crate::model::Config;
+use crate::routes::*;
 
 use std::env;
 
-use actix_web::{middleware, web, App, HttpResponse, HttpServer};
-use actix_web::http::header;
-use actix_identity::{Identity, CookieIdentityPolicy, IdentityService};
+use actix_web::{middleware, web, App, HttpServer};
+use actix_identity::{CookieIdentityPolicy, IdentityService};
 use actix_ratelimit::{RateLimiter, RedisStore, RedisStoreActor};
 
 use time::Duration;
 use handlebars::Handlebars;
 use darkredis::ConnectionPool;
 use toml::Value;
-use sqlx::postgres::{
-    PgPoolOptions,
-    PgPool,
-};
+use sqlx::postgres::PgPoolOptions;
 
 use tokio::fs::File;
 use tokio::prelude::*;
-
-#[derive(Deserialize, Serialize, Debug)]
-struct UserInfoLogin {
-    name: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct OAuthCode {
-    code: String,
-}
-
-async fn index(id: Identity, hb: web::Data<Handlebars<'_>>, redis: web::Data<ConnectionPool>) -> HttpResponse {
-    let mut conn = redis.get().await;
-
-    if let Some(token) = id.identity() {
-        if conn.get(&token).await.unwrap().is_some() {
-            let client = reqwest::Client::new();
-            let user = client.get(&format!("{}/users/@me", API_ENDPOINT))
-                .bearer_auth(&token)
-                .send()
-                .await
-                .unwrap()
-                .json::<UserResponse>()
-                .await
-                .unwrap();
-
-            let data = serde_json::json!({
-                "name": user.username,
-                "discriminator": user.discriminator,
-            });
-
-            let body = hb.render("discord_user", &data).unwrap();
-
-            return HttpResponse::Ok().body(body)
-        }
-    }
-
-    HttpResponse::Found().header(header::LOCATION, "/login").finish()
-}
-
-async fn login(id: Identity, hb: web::Data<Handlebars<'_>>, redis: web::Data<ConnectionPool>, config: web::Data<Config>) -> HttpResponse {
-    let mut conn = redis.get().await;
-
-    if let Some(token) = id.identity() {
-        if conn.get(&token).await.unwrap().is_some() {
-            return HttpResponse::Found().header(header::LOCATION, "/").finish()
-        }
-    }
-
-    let auth_url = config.oauth2_url.to_string();
-
-    let data = serde_json::json!({
-        "auth_url": auth_url,
-    });
-
-    let body = hb.render("discord_login", &data).unwrap();
-
-    HttpResponse::Ok().body(&body)
-}
-
-async fn get_token(id: Identity, redis: web::Data<ConnectionPool>, db: web::Data<PgPool>, config: web::Data<Config>) -> HttpResponse {
-    let mut conn = redis.get().await;
-
-    if let Some(token) = id.identity() {
-        if conn.get(&token).await.unwrap().is_some() {
-            let client = reqwest::Client::new();
-            let user = client.get(&format!("{}/users/@me", API_ENDPOINT))
-                .bearer_auth(&token)
-                .send()
-                .await
-                .unwrap()
-                .json::<UserResponse>()
-                .await
-                .unwrap();
-
-
-            let query = sqlx::query!("SELECT token, is_banned FROM tokens WHERE user_id = $1 AND email = $2", user.id as i64, &user.email)
-                .fetch_optional(db.as_ref())
-                .await
-                .unwrap();
-
-            if let Some(data) = query {
-                if data.is_banned {
-                    return HttpResponse::Ok().body("Account has been banned.")
-                } else {
-                    return HttpResponse::Ok().body(data.token)
-                }
-            } else {
-                let token = gen_token(
-                    user.id, &user.email,
-                    &hex::decode(&config.secret_key).unwrap(),
-                    &hex::decode(&config.iv_key).unwrap(),
-                ).unwrap_or("null".to_string());
-
-                sqlx::query!("INSERT INTO tokens (user_id, email, token) VALUES ($1, $2, $3)", user.id as i64, &user.email, &token)
-                    .execute(db.as_ref())
-                    .await
-                    .unwrap();
-
-                return HttpResponse::Ok().body(token)
-            }
-        }
-    }
-
-    HttpResponse::Ok().body("null")
-}
-
-async fn logout(id: Identity, redis: web::Data<ConnectionPool>) -> HttpResponse {
-    if let Some(token) = id.identity() {
-        let mut conn = redis.get().await;
-
-        let _ = conn.del(token).await;
-    }
-
-    id.forget();
-    HttpResponse::Found().header(header::LOCATION, "/").finish()
-}
-
-async fn oauth(code: web::Query<OAuthCode>, id: Identity, redis: web::Data<ConnectionPool>, config: web::Data<Config>) -> ServiceResult<HttpResponse> {
-    let code = code.code.to_string();
-
-    let client_id = config.client_id;
-    let client_secret = config.client_secret.to_string();
-    let redirect_uri = config.redirect_uri.to_string();
-
-    let data = OAuthTokenData {
-        client_id,
-        client_secret,
-        code,
-        redirect_uri,
-        scope: "identify email guilds".to_string(),
-        grant_type: "authorization_code".to_string(),
-    };
-
-    let client = reqwest::Client::new();
-    let resp = match client.post(&format!("{}/oauth2/token", API_ENDPOINT))
-        .form(&data)
-        .send()
-        .await
-        .unwrap()
-        .json::<OAuthResponse>()
-        .await {
-            Ok(x) => x,
-            Err(why) => {
-                return Err(ServiceError::BadRequest(why.to_string()))
-            }
-        };
-
-    id.remember(resp.access_token.to_string());
-    let mut conn = redis.get().await;
-    conn.set_and_expire_seconds(&resp.access_token, &resp.refresh_token, resp.expires_in).await.unwrap();
-
-    Ok(HttpResponse::Found().header(header::LOCATION, "/").finish())
-}
 
 #[actix_web::main]
 #[instrument]
@@ -284,11 +126,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .with_interval(std::time::Duration::from_secs(120))
                     .with_max_requests(60)
             )
-            .service(web::resource("/").route(web::get().to(index)))
-            .service(web::resource("/login").route(web::get().to(login)))
-            .service(web::resource("/logout").to(logout))
-            .service(web::resource("/token").route(web::get().to(get_token)))
-            .service(web::resource("/discord/oauth2").route(web::get().to(oauth)))
+            .service(web::resource("/").route(web::get().to(login::index)))
+            .service(web::resource("/login").route(web::get().to(login::login)))
+            .service(web::resource("/logout").to(login::logout))
+            .service(web::resource("/token").route(web::get().to(login::get_token)))
+            .service(web::resource("/discord/oauth2").route(web::get().to(login::oauth)))
     })
     .bind(&format!("{}:{}", &config.address, &config.port))?
     .workers(config.workers)
---
+++ src/routes/login.rs
@@ -0,0 +1,165 @@
+use crate::error::*;
+use crate::model::*;
+use crate::utils::tokens::gen_token;
+
+use actix_web::{web, HttpResponse};
+use actix_web::http::header;
+use actix_identity::Identity;
+
+use handlebars::Handlebars;
+use darkredis::ConnectionPool;
+use sqlx::postgres::PgPool;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UserInfoLogin {
+    name: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct OAuthCode {
+    code: String,
+}
+
+pub async fn index(id: Identity, hb: web::Data<Handlebars<'_>>, redis: web::Data<ConnectionPool>) -> HttpResponse {
+    let mut conn = redis.get().await;
+
+    if let Some(token) = id.identity() {
+        if conn.get(&token).await.unwrap().is_some() {
+            let client = reqwest::Client::new();
+            let user = client.get(&format!("{}/users/@me", API_ENDPOINT))
+                .bearer_auth(&token)
+                .send()
+                .await
+                .unwrap()
+                .json::<UserResponse>()
+                .await
+                .unwrap();
+
+            let data = serde_json::json!({
+                "name": user.username,
+                "discriminator": user.discriminator,
+            });
+
+            let body = hb.render("discord_user", &data).unwrap();
+
+            return HttpResponse::Ok().body(body)
+        }
+    }
+
+    HttpResponse::Found().header(header::LOCATION, "/login").finish()
+}
+
+pub async fn login(id: Identity, hb: web::Data<Handlebars<'_>>, redis: web::Data<ConnectionPool>, config: web::Data<Config>) -> HttpResponse {
+    let mut conn = redis.get().await;
+
+    if let Some(token) = id.identity() {
+        if conn.get(&token).await.unwrap().is_some() {
+            return HttpResponse::Found().header(header::LOCATION, "/").finish()
+        }
+    }
+
+    let auth_url = config.oauth2_url.to_string();
+
+    let data = serde_json::json!({
+        "auth_url": auth_url,
+    });
+
+    let body = hb.render("discord_login", &data).unwrap();
+
+    HttpResponse::Ok().body(&body)
+}
+
+pub async fn get_token(id: Identity, redis: web::Data<ConnectionPool>, db: web::Data<PgPool>, config: web::Data<Config>) -> HttpResponse {
+    let mut conn = redis.get().await;
+
+    if let Some(token) = id.identity() {
+        if conn.get(&token).await.unwrap().is_some() {
+            let client = reqwest::Client::new();
+            let user = client.get(&format!("{}/users/@me", API_ENDPOINT))
+                .bearer_auth(&token)
+                .send()
+                .await
+                .unwrap()
+                .json::<UserResponse>()
+                .await
+                .unwrap();
+
+
+            let query = sqlx::query!("SELECT token, is_banned FROM tokens WHERE user_id = $1 AND email = $2", user.id as i64, &user.email)
+                .fetch_optional(db.as_ref())
+                .await
+                .unwrap();
+
+            if let Some(data) = query {
+                if data.is_banned {
+                    return HttpResponse::Ok().body("Account has been banned.")
+                } else {
+                    return HttpResponse::Ok().body(data.token)
+                }
+            } else {
+                let token = gen_token(
+                    user.id, &user.email,
+                    &hex::decode(&config.secret_key).unwrap(),
+                    &hex::decode(&config.iv_key).unwrap(),
+                ).unwrap_or("null".to_string());
+
+                sqlx::query!("INSERT INTO tokens (user_id, email, token) VALUES ($1, $2, $3)", user.id as i64, &user.email, &token)
+                    .execute(db.as_ref())
+                    .await
+                    .unwrap();
+
+                return HttpResponse::Ok().body(token)
+            }
+        }
+    }
+
+    HttpResponse::Ok().body("null")
+}
+
+pub async fn logout(id: Identity, redis: web::Data<ConnectionPool>) -> HttpResponse {
+    if let Some(token) = id.identity() {
+        let mut conn = redis.get().await;
+
+        let _ = conn.del(token).await;
+    }
+
+    id.forget();
+    HttpResponse::Found().header(header::LOCATION, "/").finish()
+}
+
+pub async fn oauth(code: web::Query<OAuthCode>, id: Identity, redis: web::Data<ConnectionPool>, config: web::Data<Config>) -> ServiceResult<HttpResponse> {
+    let code = code.code.to_string();
+
+    let client_id = config.client_id;
+    let client_secret = config.client_secret.to_string();
+    let redirect_uri = config.redirect_uri.to_string();
+
+    let data = OAuthTokenData {
+        client_id,
+        client_secret,
+        code,
+        redirect_uri,
+        scope: "identify email guilds".to_string(),
+        grant_type: "authorization_code".to_string(),
+    };
+
+    let client = reqwest::Client::new();
+    let resp = match client.post(&format!("{}/oauth2/token", API_ENDPOINT))
+        .form(&data)
+        .send()
+        .await
+        .unwrap()
+        .json::<OAuthResponse>()
+        .await {
+            Ok(x) => x,
+            Err(why) => {
+                return Err(ServiceError::BadRequest(why.to_string()))
+            }
+        };
+
+    id.remember(resp.access_token.to_string());
+    let mut conn = redis.get().await;
+    conn.set_and_expire_seconds(&resp.access_token, &resp.refresh_token, resp.expires_in).await.unwrap();
+
+    Ok(HttpResponse::Found().header(header::LOCATION, "/").finish())
+}
