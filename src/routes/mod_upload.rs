c8027ff6fea460267bb565040ac5cbb4881b7a02
nitsuga5124 <nitsuga5124@gmail.com>
2020-12-13 22:03:32 +0100

    Move temporary files to storage 	modified:   .gitignore 	modified:   src/routes/mod_upload.rs

--- src/routes/mod_upload.rs
+++ src/routes/mod_upload.rs
@@ -1,11 +1,13 @@
 use crate::model::Config;
 
 use actix_web::{web, HttpResponse, Result};
-use futures::{StreamExt, TryStreamExt};
 use actix_multipart::Multipart;
 
+use futures::{StreamExt, TryStreamExt};
+use crypto::{digest::Digest, sha2::Sha256};
 use tokio::fs::File;
 use tokio::prelude::*;
+use sqlx::PgPool;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UploadJsonData {
@@ -14,6 +16,7 @@ pub struct UploadJsonData {
 
 pub async fn upload(
     config: web::Data<Config>,
+    db: web::Data<PgPool>,
     mut payload: Multipart
 ) -> Result<HttpResponse> {
     let mut contents = String::new();
@@ -27,16 +30,37 @@ pub async fn upload(
                 let data = chunk.unwrap().to_vec();
                 contents += &String::from_utf8(data).unwrap();
             }
-        } else {
+        } else if filename.ends_with(".zip") {
             let filepath = format!("{}/{}", config.mods_path, sanitize_filename::sanitize(&filename));
+            {
+                let mut f = File::create(&filepath).await?;
 
-            let mut f = File::create(filepath).await.unwrap();
-
-            // Field in turn is stream of *Bytes* object
-            while let Some(chunk) = field.next().await {
-                let data = chunk.unwrap();
-                f.write_all(&data).await?;
+                // Field in turn is stream of *Bytes* object
+                while let Some(chunk) = field.next().await {
+                    let data = chunk.unwrap();
+                    f.write_all(&data).await?;
+                }
             }
+
+            let checksum = {
+                let mut buffer = Vec::new();
+                let mut f = File::open(&filepath).await?;
+
+                f.read(&mut buffer).await?;
+
+                let mut hasher = Sha256::new();
+                hasher.input(&buffer);
+                hasher.result_str()
+            };
+
+            let first = checksum.chars().next().unwrap();
+            let second = {
+                let mut x = first.to_string();
+                x.push(checksum.chars().nth(1).unwrap());
+                x
+            };
+
+            tokio::fs::rename(&filepath, &format!("./files/{}/{}/{}.zip", first, second, checksum)).await?;
         }
     }
 
