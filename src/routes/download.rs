8fda253a6f6eb89354699c661d047b9132de7859
nitsuga5124 <nitsuga5124@gmail.com>
2020-12-30 05:36:50 +0100

    Added sanity checks to the download endpoint 	modified:   src/routes/download.rs

--- src/routes/download.rs
+++ src/routes/download.rs
@@ -1,4 +1,4 @@
-use crate::error::ServiceResult;
+use crate::error::{ServiceResult, ServiceError};
 use actix_files::NamedFile;
 use actix_web::web;
 use sqlx::PgPool;
@@ -7,6 +7,14 @@ pub async fn download(
     checksum: web::Path<String>,
     db: web::Data<PgPool>,
 ) -> ServiceResult<NamedFile> {
+    match hex::decode(&*checksum) {
+        Ok(x) if x.len() != 32 => {
+            return Err(ServiceError::BadRequest("Invalid checksum length".into()))
+        }
+        Err(_) => return Err(ServiceError::BadRequest("Invalid characters found in checksum".into())),
+        _ => (),
+    }
+
     sqlx::query!(
         "UPDATE mods SET downloads = downloads + 1 WHERE checksum = $1",
         &*checksum
