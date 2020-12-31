655dec01aeaaa110e505509ac92cbc1a7d382232
Conqu3red <46159994+Conqu3red@users.noreply.github.com>
2020-12-31 11:16:14 +0000

    Various frontend bug fixes (#22)
    * mode_view_page no longer needs data_string
    search reloads properly
    remove categories
    search results redirect to correct mod version
    move verification alert
    show message if there are no search results
    
    * redirect to correct mod from search
    
    Co-authored-by: Conqu3red <Conqu3red@users.noreply.github.com>

--- static/mod_view.js
+++ static/mod_view.js
@@ -1,25 +1,24 @@
 // Mod view script to fill in information like warnings etc.
 //console.log(document.getElementById("data_").innerText)
-data = JSON.parse(document.getElementById("data_").innerText);
-if (data.authors == undefined) data.authors = []
-if (data.categories == undefined) data.categories = []
-if (data.readme == undefined) data.readme = ""
+//categories, verification, files, release_date
+verif_detail = document.getElementById("verification_level")
+verification_level = verif_detail.innerText
+files = document.getElementById("files").innerHTML.split("<br>")
+uploaded_string = document.getElementById("uploaded").innerText
 // set readme
-if (data.readme) {
-  document.getElementById("content").innerHTML = marked(data.readme);
-}
+document.getElementById("content").innerHTML = marked(document.getElementById("content").innerHTML);
 
-document.getElementById("detail_authors").innerHTML = safetext(data["authors"].join(", "));
-document.getElementById("detail_categories").innerHTML = safetext(data["categories"].join(", "));
-document.getElementById("verification_level").innerHTML = verificationProperties.fromVerificationLevel(data["verification"]).badge;
-document.getElementById("download_button").classList.add(verificationProperties.fromVerificationLevel(data["verification"]).download_colour);
+level = verificationProperties.fromVerificationLevel(verification_level);
 
-document.getElementById("download_button").setAttribute("onclick", `document.location.href = '${data.files[0]}'`);
+verif_detail.innerHTML = level.badge
+document.getElementById("download_button").classList.add(level.download_colour);
 
-var uploaded = new Date(data.uploaded);
+document.getElementById("download_button").setAttribute("onclick", `document.location.href = '${files[0]}'`);
+
+var uploaded = new Date(uploaded_string);
 document.getElementById("release_date").innerHTML = uploaded.toLocaleDateString()
 
 
-if (verificationProperties.fromVerificationLevel(data["verification"]).alert) {
-  document.getElementById("verification_warnings").innerHTML = verificationProperties.fromVerificationLevel(data["verification"]).alert;
+if (level.alert) {
+  document.getElementById("verification_warnings").innerHTML = level.alert;
 }
